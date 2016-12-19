419f649b3ccb6bb57d96075ffc0dee297498831b
Karthik <karthikramachandra93@gmail.com>
2016-12-19 16:15:27 +0530

    added update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -1,36 +1,31 @@
-
-const {MongoClient, ObjectID} = require('mongodb');
-
-
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
-    if(err){
-       return console.log('Unable to connect to MongoDB server');
+const {
+    MongoClient, ObjectID
+} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
     }
-    
     console.log('Connected to MongoDB server');
-   
     db.collection('Todos').findOneAndUpdate({
-        _id: new ObjectID('5857aebcf3c3cc86f6d1bb3a')}, {
-        $set :{
+        _id: new ObjectID('5857aebcf3c3cc86f6d1bb3a')
+    }, {
+        $set: {
             completed: true
         }
-    },{
-        returnOriginal:false
-    }).then((result)=>{
+    }, {
+        returnOriginal: false
+    }).then((result) => {
         console.log(result);
     });
-    
-    
-    db.collection('Users').findOneAndUpdate({name:'Karthik'},
-                                            {
-        $inc:{
-            age:-2
+    db.collection('Users').findOneAndUpdate({
+        name: 'Karthik'
+    }, {
+        $inc: {
+            age: -2
         }
-    },{
-        returnOriginal:false
-    }).then((result)=>{
+    }, {
+        returnOriginal: false
+    }).then((result) => {
         console.log(result);
     });
-    
-   
-    });
+});
