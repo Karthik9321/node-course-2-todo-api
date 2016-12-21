a7a418c36c927ca197aebfab18e561754c3091c2
Karthik <karthikramachandra93@gmail.com>
2016-12-21 17:34:31 +0530

    added token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,42 +1,88 @@
 const mongoose = require('mongoose');
-var User = mongoose.model('User', {
-    First_Name:{
-        type:String,
-        required:true,
-        trim:true,
-        minlength:1
-    },
-    Last_Name: {
-    type:String,
-    required : true,
-    trim:true,
-    minlength:1
-    },
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+
+var UserSchema = new mongoose.Schema({
+//  firstName:{
+//        type: String,
+//        required:true,
+//        trim: true
+//    },
+//    
+//    lastName:{
+//        type: String,
+//        required: true,
+//        trim: true
+//    },        
     
     email:{
-      type: String,
-      required:true,
-      trim:true,
-      minlength:5
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator:  validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
     
     
-    age:{
-        type:Number
+//    phone:{
+//        type:Number,
+//        required:true,
+//        trim:true,
+//        validate: {
+//            validator: (value)=>{
+//            return validator.isMobilePhone(value+'', 'en-IN');
+//        },
+//            message: '{VALUE} is not a valid phone number'
+//        }
+//    },
+    
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
     },
     
-    Home_Address:{
-        type:String,
-        minlength: 10
-    },
-    
-    Office_Address:{
-        type:String,
-        minlength:10,
-    }
-
+    tokens: [{
+        access:{
+            type: String,
+            required: true
+        },
+        token:{
+            type: String,
+            required: true
+        }
+    }]
 
 });
 
+
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    
+    return _.pick(userObject,['_id','email']);
+};
+
+
+
+UserSchema.methods.generateAuthToken = function(){
+    var user = this;
+    var access = 'auth';
+    var token= jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    
+    user.tokens.push({access,token});
+    
+    return user.save().then(()=>{
+        return token;
+    });
+};
+var User = mongoose.model('User', UserSchema);
 
 module.exports={User};
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID}=require('mongodb');
 const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 var {mongoose}=require('./db/mongoose');
 var{Todo} = require('./models/todo');
@@ -103,6 +104,24 @@ app.patch('/todos/:id', (req,res)=>{
         res.status(400).send();
     });
 });
+
+
+//POST/users
+
+
+app.post('/users', (req,res)=>{
+    var body = _.pick(req.body,['email','password']);
+    var user =new User(body);
+    
+    user.save().then((user)=>{
+        return user.generateAuthToken();
+    }).then((token)=>{
+        res.header('x-auth', token).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
 
 
 app.listen(port, ()=>{
