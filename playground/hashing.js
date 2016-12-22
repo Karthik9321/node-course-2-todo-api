17736a7e7c19b2bdded38090c09cdb185ba365bf
Karthik <karthikramachandra93@gmail.com>
2016-12-22 11:31:25 +0530

    hashed user pass

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,7 @@
 const{SHA256}= require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 
 var message = 'I am user number 3';
 var hash = SHA256(message).toString();
@@ -18,3 +20,18 @@ console.log(token);
 
 var decoded=jwt.verify(token, '123abc');
 console.log('decoded', decoded);
+
+
+
+var password = '123abc!';
+bcrypt.genSalt(10, (err, salt)=>{
+   bcrypt.hash(password,salt,(err,hash)=>{
+      console.log(hash); 
+   }); 
+});
+
+
+var hashedPassword = '$2a$10$rrMhQQ7hmdeKUriE2szlw.2e4LiX9PNRFWfc7cOeQHL1cpp5xEMF2';
+bcrypt.compare(password, hashedPassword, (err,res)=>{
+   console.log(res); 
+});
